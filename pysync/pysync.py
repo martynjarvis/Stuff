19a7eb787c1f917308017b61787fbd27dfeb76b0
Martyn Jarvis <martynjarvis@gmail.com>
2015-01-26 15:55:36 +0000

    added pysync script

---
+++ pysync/pysync.py
@@ -0,0 +1,47 @@
+"""Wrapper for rsync.
+Expects a file called .sync that contains a destination on each line.
+Expects a .syncignore file that contains a list of patterns to ignore.
+"""
+#!/usr/bin/env python
+import os
+import subprocess
+import optparse
+
+SYNCFILE = '.sync'
+IGNOREFILE = '.syncignore'
+RSYNC = 'rsync'
+
+def suffix(string, sfx):
+    """Ensure that string endswith sfx"""
+    if not string.endswith(sfx):
+        return string+sfx
+    return string
+
+def main():
+    """Parse command line options"""
+    parser = optparse.OptionParser()
+    # TODO add some options here
+
+    (options, args) = parser.parse_args()
+
+    cwd = os.getcwd()
+    with open(os.path.join(cwd, SYNCFILE)) as sync_file:
+        if 'push' in args:
+            source = suffix(cwd, os.sep)
+            for line in sync_file:
+                dest = line.strip()
+                ret = subprocess.call([RSYNC, '-avz', '--exclude-from',
+                                       IGNOREFILE, source, dest])
+            return ret
+        elif 'pull' in args:
+            # Just pull from first destination (Think about this)
+            dest = cwd
+            source = suffix(sync_file.readline().strip(), os.sep)
+            ret = subprocess.call([RSYNC, '-avz', '--exclude-from',
+                                   IGNOREFILE, source, dest])
+            return ret
+        else :
+            return
+
+if __name__ == '__main__':
+    main()
